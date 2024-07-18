00f56b035748793ba563e02bae92642a28934ebe
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-07-18 15:27:16 +0700

    add missing repo annotation in plan repo

--- src/main/java/com/cancogang69/todo/repository/PlanRepository.java
+++ src/main/java/com/cancogang69/todo/repository/PlanRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.cancogang69.todo.entity.Plan;
 
+@Repository
 public interface PlanRepository extends JpaRepository<Plan, Integer> {
   
   @Query(value = "SELECT * FROM plans WHERE owner_id = ?", nativeQuery = true)
