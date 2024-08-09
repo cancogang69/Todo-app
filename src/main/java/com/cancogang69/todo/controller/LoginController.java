7b66ff7cd59ea7e83bdbfefbcec5046cad66596e
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-08-09 11:03:45 +0700

    add another blank line for nothing

--- src/main/java/com/cancogang69/todo/controller/LoginController.java
+++ src/main/java/com/cancogang69/todo/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.cancogang69.todo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class LoginController {
   
