2a8a1850754aea12c13ea43215c5c1186df5823d
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-08-12 21:49:20 +0700

    add function to convert from json string

--- src/main/java/com/cancogang69/todo/enums/TaskStatus.java
+++ src/main/java/com/cancogang69/todo/enums/TaskStatus.java
@@ -4,4 +4,11 @@ public enum TaskStatus {
   INITIAL,
   ONGOING,
   COMPLETED;
+
+  public static TaskStatus fromJson(String jsonStr) {
+    String regex = "[{}:\"]";
+    String[] fields = jsonStr.split(regex);
+    int statusValueIdx = 5;
+    return TaskStatus.valueOf(fields[statusValueIdx]);
+  }
 }
